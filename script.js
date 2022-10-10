9508d32bbc1b5d2cb1d8de4942aac56823bbf6a3
oludare89 <azizoyekan@hotmail.com>
2022-10-10 20:33:10 +0000

    Added gamespeed variable and background layer images

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = cavas.height = 700;
+let gameSpeed = 5;
+
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = 'images/later-1.png';
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = 'images/later-2.png';
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = 'images/later-3.png';
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = 'images/later-4.png';
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = 'images/later-5.png';
+
