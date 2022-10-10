143563d39bacfd4592ece7c8fd1aaa55c908bb13
oludare89 <azizoyekan@hotmail.com>
2022-10-10 21:00:44 +0000

    Added reloop functionality for background scroll

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ let x = 0;
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.drawImage(backgroundLayer4, x ,0);
-    x -= gameSpeed;
+    if (x < -2400) x = 2400;
+    else x -= gameSpeed;
     requestAnimationFrame(animate);
 }
 animate();
