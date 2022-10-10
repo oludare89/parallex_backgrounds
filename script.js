3a8464073b1b5c12783a6d69c8a7d9741903076e
oludare89 <azizoyekan@hotmail.com>
2022-10-10 20:51:03 +0000

    created animate function to draw background layer

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
-const CANVAS_HEIGHT = cavas.height = 700;
+const CANVAS_HEIGHT = canvas.height = 700;
 let gameSpeed = 5;
 
 const backgroundLayer1 = new Image();
-backgroundLayer1.src = 'images/later-1.png';
+backgroundLayer1.src = 'images/layer-1.png';
 const backgroundLayer2 = new Image();
-backgroundLayer2.src = 'images/later-2.png';
+backgroundLayer2.src = 'images/layer-2.png';
 const backgroundLayer3 = new Image();
-backgroundLayer3.src = 'images/later-3.png';
+backgroundLayer3.src = 'images/layer-3.png';
 const backgroundLayer4 = new Image();
-backgroundLayer4.src = 'images/later-4.png';
+backgroundLayer4.src = 'images/layer-4.png';
 const backgroundLayer5 = new Image();
-backgroundLayer5.src = 'images/later-5.png';
+backgroundLayer5.src = 'images/layer-5.png';
 
+function animate(){
+    ctx.drawImage(backgroundLayer4, 0 ,0);
+    requestAnimationFrame(animate);
+}
+animate();
