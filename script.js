cb30734f3182e1ef2562a2e2945b202ce3d1c978
oludare89 <azizoyekan@hotmail.com>
2022-10-12 15:51:23 +0000

    Created gameobjects array to draw each background layer

--- script.js
+++ script.js
@@ -43,12 +43,20 @@ class Layer {
     }
 }
 
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.7);
 const layer4 = new Layer(backgroundLayer4, 0.3);
+const layer5 = new Layer(backgroundLayer5, 0.9);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    layer4.update();
-    layer4.draw();
+    gameObjects.forEach(object => {
+        object.update();
+        object.draw();
+    })
     requestAnimationFrame(animate);
 }
 animate();
